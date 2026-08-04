118778ef2606400adf8bf587e5e3f62a62ba7d96
Nayab Yaseen Khan <nayabyaseenkhan@gmail.com>
2026-08-04 12:41:14 +0530

    Evaluate Logistic Regression model

--- train.py
+++ train.py
@@ -1,6 +1,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    classification_report
+)
 
 # Load the dataset
 df = pd.read_csv("data/train.csv")
@@ -87,3 +92,20 @@ model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
 
 print("\n Model trained successfully!")
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+print("\nPredictions:")
+print(y_pred[:10])
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("\nModel Accuracy:")
+print(f"{accuracy:.2%}")
+
+print("\nConfusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
+
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
